fb57b980e096b394295daac007f47a1c96e42c9c
Brian Davis <davistib@gmail.com>
2022-05-30 20:35:09 -0600

    update test

--- ch13/array/src/concrete_column_major_subscriptor_test.cpp
+++ ch13/array/src/concrete_column_major_subscriptor_test.cpp
@@ -14,8 +14,17 @@ TEST_CASE( "Create a 1D column major subscriptor", "[]" ) {
 
     auto c = ConcreteColumnMajorSubscriptor(SubscriptArray<1>(5));
 
-    auto subscript = SubscriptArray<1>(2);
-    REQUIRE( c.offset(subscript) == subscript(0) );
+    SECTION( "Computes the offset" ) {
+        auto subscript = SubscriptArray<1>(2);
+        REQUIRE( c.offset(subscript) == subscript(0) );
+        REQUIRE( c.offset(subscript) == 2 );
+    }
+
+    SECTION( "Computes the offset outside bounds" ) {
+        auto subscript = SubscriptArray<1>(6);
+        REQUIRE( c.offset(subscript) == subscript(0) );
+        REQUIRE( c.offset(subscript) == 6 );
+    }
 
 }
 
@@ -30,6 +39,9 @@ TEST_CASE( "Create a 2D column major subscriptor", "[]" ) {
     REQUIRE( c.offset(subscript) == 5 );  // Note array is only size 3
 
     subscript = SubscriptArray<2>(0, 1);
-    REQUIRE( c.offset(subscript) == 5 );
+    REQUIRE( c.offset(subscript) == 3 );
+
+    subscript = SubscriptArray<2>(2, 3);
+    REQUIRE( c.offset(subscript) == 11 );
 
 }
